6ca3fb0052313b5f0d53948a7166575e139ee5ea
shinyahirai <908.shinya@gmail.com>
2018-02-22 14:08:00 +0800

    index完成

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
     echo 'Hello world';
     var_dump(1);
+
+    $name = 'Shinya Hirai';
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  <h1>Hello Github!</h1>
+</body>
+</html>
